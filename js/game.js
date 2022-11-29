9358d4def422083cbd5450d4cc91f2e15ad75d04
CptNekcmanINT <neckman@yandex.ru>
2022-11-30 00:49:48 +0300

    first commit

---
+++ js/game.js
@@ -0,0 +1,13 @@
+const b1 = document.getElementById("b1-1");
+const b2 = document.getElementById('b1-2');
+const b3 = document.getElementById('b1-3');
+const b4 = document.getElementById('b2-1');
+const b5 = document.getElementById('b2-2');
+const b6 = document.getElementById('b2-3');
+const b7 = document.getElementById('b3-1');
+const b8 = document.getElementById('b3-2');
+const b9 = document.getElementById('b3-3');
+const cross = "X";
+const nought = "O";
+
+let block_value = [10];
